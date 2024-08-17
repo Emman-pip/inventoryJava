38818428f5886bb3c3da4e3c0f47ef67bcbd9346
Emman-pip <parrenoemmanuel756@gmail.com>
2024-08-17 12:47:15 +0800

    Add login and signup ui.

--- app/src/main/java/com/main/App.java
+++ app/src/main/java/com/main/App.java
@@ -9,7 +9,7 @@ public class App extends Application {
 		new LoginSignUpWindow();
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		launch(args);
 	}
 }
--- app/src/main/java/com/main/LoginSignUpWindow.java
+++ app/src/main/java/com/main/LoginSignUpWindow.java
@@ -1,32 +1,83 @@
 package com.main;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.Objects;
 
-import javafx.scene.Group;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class LoginSignUpWindow extends Stage {
 
-	LoginSignUpWindow() throws Exception {
+	LoginSignUpWindow() {
 		this.setScene(mainGrid());
 		this.setTitle("Login");
 		this.show();
 	}
 
-	private Scene mainGrid() throws FileNotFoundException {
-		GridPane pane = new GridPane();
-		Scene scene = new Scene(pane);
+	private GridPane LoginForm() {
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(10, 10, 10, 10));
+		grid.setVgap(10);
+		grid.setHgap(10);
+		grid.setAlignment(Pos.CENTER);
+
+		Label username_lbl = new Label("Username ");
+		TextField username_field = new TextField();
+		Label password_lbl = new Label("Password");
+		PasswordField password_field = new PasswordField();
+
+		// set up buttons
+		HBox vbox = new HBox();
+		vbox.setAlignment(Pos.CENTER_RIGHT);
+		vbox.setFillHeight(true);
+
+		Button login_btn = new Button();
+		login_btn.setText("Login");
+		login_btn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("This will be connected to other class' method");
+			}
+		});
+
+		Button signup_btn = new Button();
+		signup_btn.setText("Signup");
+		signup_btn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("This will be connected to other class' method");
+			}
+		});
+
+		vbox.getChildren().add(signup_btn);
+		vbox.getChildren().add(login_btn);
+		signup_btn.setCursor(Cursor.HAND);
+		login_btn.setCursor(Cursor.HAND);
+
+		grid.add(username_lbl, 1, 1);
+		grid.add(username_field, 2, 1);
+		grid.add(password_lbl, 1, 2);
+		grid.add(password_field, 2, 2);
+		grid.add(vbox, 1, 3, 2, 1);
+
+		return grid;
+	}
+
+	private Scene mainGrid() {
+		GridPane grid = LoginForm();
+
+		Scene scene = new Scene(grid, 500, 500);
 		return scene;
 	}
 }
