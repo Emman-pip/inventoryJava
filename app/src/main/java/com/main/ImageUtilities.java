ce4d75e2bec0bc148b7d238c7c33b600f74c1c1b
Emman-pip <parrenoemmanuel756@gmail.com>
2024-08-17 12:48:20 +0800

    Create a class for image loading.

---
+++ app/src/main/java/com/main/ImageUtilities.java
@@ -0,0 +1,26 @@
+package com.main;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class ImageUtilities {
+	public ImageView loadImage(String imageName) throws FileNotFoundException {
+		return new ImageView(
+				new Image(new FileInputStream(new File(App.class.getResource(imageName).toString().substring(5)))));
+	}
+
+	public String filePath(String fileName) {
+		// System.out.println(App.class.getResource(fileName).toString());
+		return App.class.getResource(fileName).toString();
+	}
+
+	public boolean imageExists(String imageName) {
+		File file = new File(App.class.getResource(imageName).toString().substring(5));
+		System.out.println(file.getAbsolutePath());
+		return file.exists();
+	}
+}
