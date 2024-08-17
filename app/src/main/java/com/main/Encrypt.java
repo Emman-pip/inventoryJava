0be4baf9f56ea82acffaec5ddae90831a197a04b
Emman-pip <parrenoemmanuel756@gmail.com>
2024-08-17 18:46:45 +0800

    Make a hashing function.

---
+++ app/src/main/java/com/main/Encrypt.java
@@ -0,0 +1,14 @@
+package com.main;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+public class Encrypt {
+	public String encrypt(String code) throws Exception {
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		digest.update(code.getBytes(java.nio.charset.StandardCharsets.UTF_8));
+		byte[] hashed = digest.digest();
+		String res = String.format("%64x", new BigInteger(1, hashed));
+		return res;
+	};
+}
